06c406e8d1fe753e46ec31446cb828c9a3c55227
tadhanadestiny <156416355+tadhanadestiny@users.noreply.github.com>
2024-09-11 02:14:47 +0800

    Add files via upload

---
+++ UseOfVariable.java
@@ -0,0 +1,8 @@
+public class UseOfVariable {
+public static void main (String[] args) {
+String studentName= ("Destiny");
+int studentAge= 19;
+System.out.println ("Hello, I’m " + studentName + " and");
+System.out.println ("I'm " + studentAge + " years old");
+}
+}
